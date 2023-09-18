5025ea5e1423949f418ff6de514d7c365e6d8b86
Salmaniac Prashant <salmaniacprashant@gmail.com>
2023-09-18 14:44:02 +0530

    prometheus & grafanna setup

---
+++ grafana.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+helm repo add grafana https://grafana.github.io/helm-charts
+helm repo update
+
+helm upgrade --install loki grafana/loki-stack
+
+exit 0
---
+++ prometheus.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
+helm install kube-prometheus-stack prometheus-community/kube-prometheus-stack
+
+exit 0
